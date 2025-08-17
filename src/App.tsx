16483bcdcc0a2b2c96554a2c1d233a7f48af5a6a
Ramarao <55307774+rrdesai64@users.noreply.github.com>
2025-08-17 15:26:05 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,6 @@ import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-       
-      </ul>
-      <div>
-       
-        <br />
-        
-      </div>
-    </main>
-  );
-}
+    
 
 export default App;
