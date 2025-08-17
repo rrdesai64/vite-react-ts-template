a3ba73d9c54e1dbbfd449a3d5527c79980c69e30
Ramarao <55307774+rrdesai64@users.noreply.github.com>
2025-08-17 15:19:30 +0530

    Update resource.ts

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -6,13 +6,6 @@ adding a new "isDone" field as a boolean. The authorization rule below
 specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
-const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-    })
-    .authorization((allow) => [allow.publicApiKey()]),
-});
 
 export type Schema = ClientSchema<typeof schema>;
 
